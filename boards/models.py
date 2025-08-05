90b91baf11e776455fa526a69d54d3a149a8a467
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-08-04 23:02:26 -0400

    Fixes column type

--- boards/models.py
+++ boards/models.py
@@ -1,4 +1,5 @@
 import uuid
+import datetime
 
 from sqlalchemy import Boolean, DateTime, String
 from sqlalchemy.dialects.postgresql import UUID
@@ -14,10 +15,10 @@ class Board(Base):
     id: Mapped[uuid] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name: Mapped[str] = mapped_column(String(30), unique=True)
     description: Mapped[str] = mapped_column(String(255))
-    created_at: Mapped[DateTime.datetime] = mapped_column(
+    created_at: Mapped[datetime.datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now()
     )
-    updated_at: Mapped[DateTime.datetime] = mapped_column(
+    updated_at: Mapped[datetime.datetime] = mapped_column(
         DateTime(timezone=True), onupdate=func.now
     )
     active: Mapped[bool] = mapped_column(Boolean, default=True)
